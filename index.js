96948a5c3b4d945709bab055a15d6a815fa0d946
Muceres <samuel.joset@gmail.com>
2020-09-12 21:40:01 +0200

    feat: add sortOf function

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-const dummyFunction = _ => _;
+const R = require('ramda');
 
-module.exports = dummyFunction;
+const ASC = 'asc';
+const DESC = 'desc';
+
+const sortBy = {
+  asc: {
+    firstCondition: -1,
+    secondCondition: 1
+  },
+  desc: {
+    firstCondition: 1,
+    secondCondition: -1
+  }
+}
+
+/* Sort objects by the value of their key
+ *
+ * @param {Number[]|Object[]} arr - the array to retrieve the value from.
+ * @param {String[]} [path=[]]
+ * @param String command -
+ * @return {Number[]|Object[]} the sorted array
+ */
+const sortOf = (arr, path = [], command = ASC) => {
+  if (arr.length === 0) return [];
+
+  command = (command.toLowerCase() === ASC) ? ASC : DESC;
+
+  return arr.sort((a, b) => {
+    const elem1 = R.pathOr(a, path, a);
+    const elem2 = R.pathOr(b, path, b);
+
+    if (elem1 < elem2) return sortBy[command].firstCondition;
+    if (elem1 > elem2) return sortBy[command].secondCondition;
+
+    return 0;
+  });
+}
+
+module.exports = sortOf;
